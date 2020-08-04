6ba985e035305c60583afbc86a6c52cd1973d2c3
Svyatoslav <chel_c@mail.ru>
2020-08-04 23:42:44 +0300

    less_5

--- mutedalien/routes/web.php
+++ mutedalien/routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    return view('welcome')->with([
+        'tasks' => [
+        'Go to the store',
+        'Go to the market',
+        'Go to work'
+    ]]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/about', function () {
+    return view('about');
 });
