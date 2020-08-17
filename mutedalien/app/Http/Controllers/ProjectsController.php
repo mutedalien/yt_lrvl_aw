0f24c53b0f15244c330f7a16c7c971686b3edf5e
Svyatoslav <chel_c@mail.ru>
2020-08-17 22:03:20 +0300

    less_13

--- mutedalien/app/Http/Controllers/ProjectsController.php
+++ mutedalien/app/Http/Controllers/ProjectsController.php
@@ -54,4 +54,11 @@ class ProjectsController extends Controller
 
         return redirect("/projects");
     }
+
+    public function show($id) {
+
+        $project = Project::findOrFail($id);
+
+        return view('projects.show', compact('project'));
+    }
 }
---
+++ mutedalien/resources/views/projects/show.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+    <h1>{{ $project->title }}</h1>
+@endsection
